8f6527dba6b756eab13f19235e104ba6dbfddb60
Rooster <113987912+Roschool@users.noreply.github.com>
2024-06-16 16:35:54 +0200

    better

---
+++ artikel/0000003-Vervolg-Rusland-begint-oorlog-met-Polen/script2.js
@@ -0,0 +1,46 @@
+// Wacht tot de DOM geladen is
+document.addEventListener("DOMContentLoaded", function() {
+    // Zoek het element met de id 'text-container'
+    var textContainer = document.getElementById("text-container");
+
+    // Maak een XMLHttpRequest om de inhoud van de NOS-homepagina op te halen
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://www.nos.nl/", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Zet de ontvangen HTML in de tekstcontainer
+            textContainer.innerHTML = xhr.responseText;
+
+            // Zoek naar het element waar de tekst zich bevindt (bijvoorbeeld een <div> met een specifieke class)
+            var textElement = textContainer.querySelector('.text-container'); // Vervang 'example-class' door de daadwerkelijke class
+
+            if (textElement) {
+                // Voeg de tekst toe aan de tekstcontainer
+                textContainer.innerHTML = textElement.innerHTML;
+            } else {
+                textContainer.innerHTML = "Kan de tekst niet vinden op de pagina.";
+            }
+        }
+    };
+    xhr.send();
+});
+
+//var goBackToHomeSvgBtn = document.getElementById("close")
+
+//goBackToHomeSvgBtn.addEventListener("click", function() {
+    /// Toon het laadscherm
+    //document.getElementById("laadscherm").style.display = "flex";
+    /// Wacht 1 seconde voordat je de gebruiker doorstuurt
+    //setTimeout(function() {
+        /// Wijzig de locatie van het venster naar de gewenste URL
+        //window.location.href = "./";
+    //}, 1000); /// Laad de pagina na 1 seconde
+
+//});
+
+function goBackToHome() {
+    document.getElementById("laadscherm").style.display = "flex";
+    setTimeout(function() {
+        window.location.href= "https://europenews.github.io"
+    }, 1000);
+};
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var Article1 = document.getElementById("Article1")
 var Article2 = document.getElementById("Article2")
+var Article3 = document.getElementById("Article3")
 
 Article1.addEventListener("click", function() {
      // Toon het laadscherm
@@ -20,6 +21,18 @@ Article2.addEventListener("click", function() {
     setTimeout(function() {
         // Wijzig de locatie van het venster naar de gewenste URL
         window.location.href = "./artikel/0000002-Interview-Rusland-begint-oorlog-met-polen";
+        document.getElementById("laadscherm").style.display = "none";   
+    }, 1000); // Laad de pagina na 1 seconde           
+
+});
+
+Article3.addEventListener("click", function() {
+    // Toon het laadscherm
+    document.getElementById("laadscherm").style.display = "flex";
+    // Wacht 1 seconde voordat je de gebruiker doorstuurt
+    setTimeout(function() {
+        // Wijzig de locatie van het venster naar de gewenste URL
+        window.location.href = "./artikel/0000003-Vervolg-Rusland-begint-oorlog-met-Polen";
         document.getElementById("laadscherm").style.display = "none";   
     }, 1000); // Laad de pagina na 1 seconde           
 
