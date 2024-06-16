c6c819d1d2a2b5d8f6b7ba4f0b55819c7bf2aa2b
Rooster <113987912+Roschool@users.noreply.github.com>
2024-06-16 15:37:36 +0200

    better

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var Article1 = document.getElementById("Article1")
+var Article2 = document.getElementById("Article2")
 
 Article1.addEventListener("click", function() {
      // Toon het laadscherm
@@ -10,6 +11,18 @@ Article1.addEventListener("click", function() {
          document.getElementById("laadscherm").style.display = "none";   
      }, 1000); // Laad de pagina na 1 seconde           
  
+});
+
+Article2.addEventListener("click", function() {
+    // Toon het laadscherm
+    document.getElementById("laadscherm").style.display = "flex";
+    // Wacht 1 seconde voordat je de gebruiker doorstuurt
+    setTimeout(function() {
+        // Wijzig de locatie van het venster naar de gewenste URL
+        window.location.href = "./artikel/0000001-Rusland-begint-oorlog-met-Polen";
+        document.getElementById("laadscherm").style.display = "none";   
+    }, 1000); // Laad de pagina na 1 seconde           
+
 });
 
 // JavaScript for interactive elements
